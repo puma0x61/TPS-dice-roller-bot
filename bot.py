9028b2642b9677a49b8d774a9e20fd7b5468d425
Puma Concolor <puma.9789@gmail.com>
2020-06-14 21:50:28 +0200

    Add ability score calculator

--- bot.py
+++ bot.py
@@ -2,10 +2,21 @@
 
 import telebot
 from random import randint
+from iteration_utilities import flatten
 
-TOKEN = '1190003676:AAErBi7Sy_a2TiDzpsqEUXd6hmhQVNR2mvI'
+### TODO:
+# penis size
+# set caratteristiche
+# tit request
+
+TOKEN = '1190003676:AAFYarLWv57VUBLituGun4uLe_MD0Xs4TWg'
 
 bot = telebot.TeleBot(TOKEN)
+
+
+### roll(dice, number, mod)
+# takes three values, corresponding to: number of sides of the die, number of dice,
+# modifier to the roll, returns the tuple (result, result_list)
 
 def roll(dice, number, mod):
     result_list = []
@@ -13,6 +24,43 @@ def roll(dice, number, mod):
         result_list.append(randint(1, dice))
     result = sum(result_list) + mod
     return (result, result_list)
+
+
+### score_roll()
+
+def score_roll():
+    score_list = []
+    dice = 6
+    number = 4
+    mod = 0
+    for i in range(0, 6):
+        score_result_tmp = []
+        tmp_result, tmp_result_list = roll(dice, number, mod)
+        score_result_tmp.append(tmp_result_list)
+        score_result = remove_minimum(score_result_tmp)
+        score_list.append(score_result)
+    return_list = list(flatten([[sum(scores) for scores in sub_list] for sub_list in score_list]))
+    return return_list
+
+
+def remove_minimum(input_list):
+    output_list = []
+    for sub_list in input_list:
+        minimum_index = 0
+        new_sub_list = []
+        for i, n in enumerate(sub_list):
+            if n < sub_list[minimum_index]: minimum_index = i
+        for i, n in enumerate(sub_list):
+            if i == minimum_index:
+                continue
+            else:
+                new_sub_list.append(n)
+        output_list.append(new_sub_list)
+    return output_list
+
+
+### parse_text(text)
+# takes a string, return three values for use with roll()
 
 def parse_text(text):
     command, equation = text.split()
@@ -24,16 +72,47 @@ def parse_text(text):
         print(e)
     return (int(other), int(number), 0)
 
+
+### handle_roll(message)
+# handler for the commands /roll, /r
+
 @bot.message_handler(commands=['roll', 'r'])
 def handle_roll(message):
     try:
+        name = message.from_user.username
         dice, number, mod = parse_text(message.text)
         result, result_list = roll(dice, number, mod)    
-        response = f'{result}, ({result_list})'
+        response = f'@{name} rolled {result}, ({result_list})'
     except Exception as e:
         print(e)
+        print(message)
         response = 'Eh?'
     bot.reply_to(message, response)
+
+
+### handle_score
+
+@bot.message_handler(commands=['ability_scores', 'as'])
+def handle_score(message):
+    name = message.from_user.username
+    score_list = score_roll()
+    ability_scores = f'@{name} rolled {score_list}'
+    bot.reply_to(message, ability_scores)
+
+
+### debug: used to get file_id of a single sticker
+# @bot.message_handler(content_types=["sticker"])
+# def handle_sticker(message):
+#     print(message)
+
+
+### handle_pelor(message)
+# answers to messages containing "pelor" with the right sticker
+
+@bot.message_handler(regexp="pelor")
+def handle_pelor(message):
+    chat_id = message.chat.id
+    bot.send_sticker(chat_id, "CAACAgQAAxkBAANDXuZB7Nb-rImmxXLfiWVXqj2OG5UAAjwAAy_0Wg-jNOhAndo8mxoE")
 
 bot.polling()
 
