ba93b78249c48591fb885de2271ce98ae917e54f
Francesco Martinengo <puma.9789@gmail.com>
2020-06-19 10:00:08 +0200

    Update edgelord_feature.py

--- pumas_rollbot/core/edgelord_feature.py
+++ pumas_rollbot/core/edgelord_feature.py
@@ -5,5 +5,4 @@ from .constants import EDGELORD_PARTS
 def edgelord_feature():
     edginess = [choice(EDGELORD_PARTS[0]), choice(EDGELORD_PARTS[1]), choice(EDGELORD_PARTS[2]),
                 choice(EDGELORD_PARTS[3])]
-
     return ' '.join(edginess)
