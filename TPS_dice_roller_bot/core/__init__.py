145b150c41a23f92bb1df3b1bf3915e955ec2e79
Puma Concolor <puma.9789@gmail.com>
2020-10-01 12:44:27 +0200

    Clean __init__.py

--- TPS_dice_roller_bot/core/__init__.py
+++ TPS_dice_roller_bot/core/__init__.py
@@ -3,5 +3,3 @@ from .parse import *
 from .iamalive_feature import alive_service
 from .spongebob_feature import *
 from .edgelord_feature import *
-from .tit_request_feature import *
-from .tit_size_feature import *
