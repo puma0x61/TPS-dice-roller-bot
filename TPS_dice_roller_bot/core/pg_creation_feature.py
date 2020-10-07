7b4d51e064f72471097a636463ee124a278a1e49
Jacopo De Angelis <45501385+JacopoDeAngelis@users.noreply.github.com>
2020-10-07 10:43:29 +0200

    Created character_creator_feature (#29)

---
+++ TPS_dice_roller_bot/core/pg_creation_feature.py
@@ -0,0 +1,26 @@
+from random import choice
+from .constants import PG_CREATION_PARTS
+from .roll import score_roll
+
+
+def pg_creation_feature():
+    character_sheet = []
+    for key in PG_CREATION_PARTS:
+        characteristic_selected = characteristic_selector(key)
+        characteristic_message = characteristic_message_creator(key, characteristic_selected)
+        character_sheet.append(characteristic_message)
+    character_sheet.append(characteristic_scores_creator())
+    return '\n'.join(character_sheet)
+
+
+def characteristic_selector(key):
+    return choice(PG_CREATION_PARTS.get(key))
+
+
+def characteristic_message_creator(key, characteristic):
+    return key + ': ' + characteristic
+
+
+def characteristic_scores_creator():
+    scores = score_roll()
+    return f'Characteristic scores: {scores}'
